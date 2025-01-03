cad88616c073633ed6a090a30019dd990d9c334b
Sree988711 <sree.988711@gmail.com>
2025-01-03 13:18:12 +0530

    'second_page_start'

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('home/',views.index)
+    path('home/',views.index,name='home'),
+    path('courses/',views.courses,name='courses'),
 ]
