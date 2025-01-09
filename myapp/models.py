00ec64243015dd4ce1f0a40bc1428fa207264c93
Sree988711 <sree.988711@gmail.com>
2025-01-09 14:31:45 +0530

    'progress'

--- myapp/models.py
+++ myapp/models.py
@@ -14,3 +14,17 @@ class testing_courses(models.Model):
         verbose_name_plural='Testing Courses'
     def __str__(self):
         return self.course_name
+
+class networking_courses(models.Model):
+    course_name=models.CharField(max_length=50)
+    class Meta:
+        verbose_name_plural='Networking Courses'
+    def __str__(self):
+        return self.course_name
+
+class other_courses(models.Model):
+    course_name=models.CharField(max_length=50)
+    class Meta:
+        verbose_name_plural='Other Courses'
+    def __str__(self):
+        return self.course_name
--- myapp/views.py
+++ myapp/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render
-from .models import courses,testing_courses
+from .models import courses,testing_courses,networking_courses,other_courses
 # Create your views here.
 def index(request):
     return render(request,'index.html')
 def course(request):
     details=courses.objects.all()
     testing=testing_courses.objects.all()
+    networking=networking_courses.objects.all()
+    other=other_courses.objects.all()
     return render(request,'courses.html',{
         'details':details,
         'testing':testing,
+        'networking':networking,
+        'other':other
         })
