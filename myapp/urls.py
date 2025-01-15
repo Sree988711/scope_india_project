3f64b69958d1a87c41a519420c9c4595ac914425
Sree988711 <sree.988711@gmail.com>
2025-01-15 13:59:39 +0530

    last_page_complete

--- myapp/urls.py
+++ myapp/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('courses/',views.course,name='courses'),
     path('course-details/<str:category>/<int:id>/',views.course_details,name='detailx'),
     path('about/',views.about,name='about'),
-    path('contact/',views.contact,name='contact')
+    path('contact/',views.contact,name='contact'),
+    path('register/',views.register,name='register')
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -37,3 +37,5 @@ def about(request):
     return render(request,'about.html')
 def contact(request):
     return render(request,'contact.html')
+def register(request):
+    return render(request,'register.html')
